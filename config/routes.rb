83fe33fe5c9388b15a6854caa416fd5b4f8d2bcb
LoopFruits <Kensworldc@gmail.com>
2022-04-07 10:12:47 -0400

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  resources :birds #when using all the restful routes you don't need to write them all out, its assumed at this point
   patch "/birds/:id/like", to: "birds#increment_likes"
+  
 end
